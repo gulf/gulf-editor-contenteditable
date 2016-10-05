7c7b56fdde0b21b9cfd244fe47ffe834d7d36ca8
Marcel Klehr <mklehr@gmx.net>
2016-10-05 14:37:33 +0200

    Switch to gulf v5 API

--- README.md
+++ README.md
@@ -1,26 +1,31 @@
-# gulf-contenteditable
-Convenient [gulf](http://github.com/marcelklehr/gulf#readme) wrapper for contenteditable elements
+# gulf-editor-contenteditable
+[Gulf](http://github.com/marcelklehr/gulf#readme) bindings for contenteditable elements
 
 ## Install
 
 ```
-npm install gulf-contenteditable
+npm install --save gulf gulf-editor-contenteditable dom-ot
 ```
 
 ## Usage
 
 ```
-var bindEditor = require('gulf-contenteditable')
+const domOT = require('dom-ot')
+const ContenteditableDocument = require('gulf-editor-contenteditable')
 
 var editable = document.querySelecor('#doc[contenteditable]')
-var doc = bindEditor(editable)
+var doc = new ContenteditableDocument({
+  storageAdapter: new gulf.MemoryAdapter
+, ottype: domOT
+, editorInstance: editable
+})
 ```
 
 ## API
-### bindEditor(editable:DOMElement, [storageAdapter])
-  * `editable` -- a contenteditable Element to be wired up with gulf
+### class ContenteditableDocument({editorInstance:HTMLElement,...}) extends gulf.EditableDocument
+  * `contenteditable` -- a contenteditable Element to be wired up with gulf
   * `storageAdapter` -- a gulf storage adapter (optional; defaults to the in-memory Adapter)
-  * *returns* the `gulf.EditableDocument` (see [the gulf docs](http://github.com/marcelklehr/gulf#readme))
+  * `ottype` -- the OT type, with this binding you'll want `dom-ot`
 
 
 ## Legal
---
+++ src/index.js
@@ -0,0 +1,109 @@
+/**
+ * gulf-editor-contenteditable
+ * Copyright (C) 2015-2016 Marcel Klehr <mklehr@gmx.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+var gulf = require('gulf')
+  , domOT = require('dom-ot')
+  , MutationSummary = require('mutation-summary')
+
+class ContenteditableDocument extends gulf.EditableDocument {
+  constructor(opts) {
+    super(opts)
+    if (!opts.editorInstance) throw new Error('No contenteditable HTMLElement passed')
+    this.rootNode = opts.editorInstance
+  
+    this.mutationSummary = new MutationSummary({
+      rootNode: this.rootNode, // (defaults to window.document)
+      oldPreviousSibling: true,
+      queries: [
+        { all: true}
+      ],
+      callback: (summaries) => this.onLocalChange(summaries)
+    })
+  }
+
+  close() {
+    super.close()
+    this.mutationSummary.disconnect()
+  }
+  
+  _onLocalChange(summaries) {
+    var ops = domOT.adapters.mutationSummary.import(summaries[0], this.rootNode)
+    ops = ops.filter(function(op) {
+      // filter out changes to the root node
+      if(op.path) return !!op.path.length
+      else return true
+    })
+    if(!ops.length) return
+    
+    this.submitChange(ops)
+    ops.forEach(function(op) {
+      op.apply(this.rootNode, /*index:*/true, /*dry:*/true)
+    })
+  }
+
+  _setContent(newcontent) {
+    this.mutationSummary.disconnect()
+    
+    this.rootNode.innerHTML = ''
+    for(var i=0; i<newcontent.childNodes.length; i++) {
+      this.rootNode.appendChild(newcontent.childNodes[i].cloneNode(/*deep:*/true))
+    }
+
+    domOT.adapters.mutationSummary.createIndex(contenteditable)
+    this.mutationSummary.reconnect()
+
+    return Promise.resolve()
+  }
+
+  _onChange(changes) {
+    observer.disconnect()
+
+    var ops = domOT.unpackOps(changes)
+    retainSelection(this.rootNode, ops, () => {
+      ops.forEach((op) => {
+        op.apply(this.rootNode, /*index:*/true)
+      })
+    })
+
+    observer.reconnect()
+
+    return Promise.resolve()
+  }
+
+  _onBeforeChange() {
+    return Promise.resolve()
+  }
+}
+
+module.export = ContenteditableDocument
+
+function retainSelection(rootNode, ops, fn) {
+  var selection = rootNode.ownerDocument.defaultView.getSelection()
+        , ranges = []
+  for(var i=0; i<selection.rangeCount; i++) {
+    var range = selection.getRangeAt(i)
+    ranges.push(domOT.transformCursor(range, ops, rootNode))
+  }
+  fn()
+  selection.removeAllRanges()
+  ranges.forEach(function(r) {
+    var range = new Range
+    if(r.startContainer) range.setStart(r.startContainer, r.startOffset)
+    if(r.endContainer) range.setEnd(r.endContainer, r.endOffset)
+    selection.addRange(range)
+  })
+}
